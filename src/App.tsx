69052448957f62a42c7c8e6e608fa73f20f71994
gom3aelhoot-prog <gom3a.elhoot@yandex.ru>
2026-09-26 14:26:51 +0300

    feat: route advanced search

--- src/App.tsx
+++ src/App.tsx
@@ -66,6 +66,7 @@ import NotificationsPage from '@/pages/NotificationsPage';
 import SettingsPage from '@/pages/SettingsPage';
 import MediaHubPage from '@/pages/MediaHubPage';
 import SpecialistStorePage from '@/pages/SpecialistStorePage';
+import SearchPage from '@/pages/SearchPage';
 import StoreAdminPage from '@/pages/StoreAdminPage';
 import AppsPage from '@/pages/AppsPage';
 import SpecialistContentUploadPage from '@/pages/SpecialistContentUploadPage';
@@ -108,6 +109,7 @@ function PlatformRoute() {
   if (route.startsWith('/facilities/')) return <InstitutionDetailPage kind="facility" />;
   if (route === '/media') return <MediaHubPage />;
   if (route === '/store') return <SpecialistStorePage />;
+  if (route === '/search') return <SearchPage />;
   if (route === '/cart') return <SpecialistStorePage />;
   if (route === '/admin/store') return <StoreAdminPage />;
   if (route === '/apps') return <AppsPage />;
